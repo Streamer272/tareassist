5408edf2637cd9f5287e2708a0371fb9a3e2b393
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-03-26 12:20:44 -0700

    :zap: Adds react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { NotFound } from "./pages/NotFound";
+import { Landing } from "./pages/Landing";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <App>
+      <RouterProvider router={router} />
+    </App>
+  </React.StrictMode>
+);
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+export function NotFound() {
+  return (
+    <>
+      <p className="title">404</p>
+      <p className="subtitle">Page not found</p>
+      <p className="link">
+        Return <a href="/">home</a>
+      </p>
+    </>
+  );
+}
